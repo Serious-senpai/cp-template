e3bf5aab8e4dbc870c028ad408f1b20a8b0e3e33
Haruka3399 <57554044+Saratoga-CV6@users.noreply.github.com>
2022-10-29 18:49:48 +0700

    Add templates

---
+++ multitest-bool.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+
+#define int long long
+#define INF LLONG_MAX
+
+using namespace std;
+
+template <typename T>
+ostream &operator<<(ostream &out, const vector<T> &_v)
+{
+    int n = _v.size();
+    out << "[";
+    for (int i = 0; i < n; i++)
+    {
+        out << _v[i];
+        if (i < n - 1)
+        {
+            out << ", ";
+        }
+    }
+    out << "]";
+
+    return out;
+}
+
+bool solve()
+{
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int tests;
+    cin >> tests;
+    for (int _t = 0; _t < tests; _t++)
+    {
+        cout << (solve() ? "YES" : "NO") << "\n";
+    }
+}
