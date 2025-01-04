82727e64e40f0adeeeb2989495b607d336ad840d
Serious-senpai <57554044+Serious-senpai@users.noreply.github.com>
2025-01-04 13:33:15 +0700

    Update template

--- general.cpp
+++ general.cpp
@@ -1,9 +1,8 @@
-#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("O3")
+
 #include <bits/stdc++.h>
 
 #define int long long
-#define INF LLONG_MAX
-#define inf 1000000007
 
 using namespace std;
 
@@ -15,87 +14,155 @@ void pipe(const char *file_name)
     freopen(file_name, "r", stdin);
 }
 
-template <typename T>
-void list_elements(ostream &stream, const vector<T> &_v)
-{
-    int n = _v.size();
-    for (int i = 0; i < n; i++)
-    {
-        stream << _v[i];
-        if (i < n - 1)
-        {
-            stream << ", ";
-        }
-    }
-}
-
-template <typename T>
-ostream &operator<<(ostream &stream, const vector<T> &_v)
-{
-    stream << "[";
-    list_elements(stream, _v);
-    stream << "]";
-
-    return stream;
-}
-
-template <typename T, typename Compare, typename Alloc>
-ostream &operator<<(ostream &stream, const set<T, Compare, Alloc> &_s)
-{
-    vector<T> _v(_s.begin(), _s.end());
-
-    stream << "{";
-    list_elements(stream, _v);
-    stream << "}";
-
-    return stream;
-}
-
-template <typename T, typename Compare, typename Alloc>
-ostream &operator<<(ostream &stream, const multiset<T, Compare, Alloc> &_s)
-{
-    vector<T> _v(_s.begin(), _s.end());
-
-    stream << "{";
-    list_elements(stream, _v);
-    stream << "}";
-
-    return stream;
-}
-
-template <typename T1, typename T2, typename Compare, typename Alloc>
-ostream &operator<<(ostream &stream, const map<T1, T2, Compare, Alloc> &_m)
-{
-    vector<pair<T1, T2>> _v(_m.begin(), _m.end());
-
-    stream << "{";
-    int n = _v.size();
-    for (int i = 0; i < n; i++)
-    {
-        stream << _v[i].first << ": " << _v[i].second;
-        if (i < n - 1)
-        {
-            stream << ", ";
-        }
-    }
-    stream << "}";
-
-    return stream;
-}
-
-template <typename T1, typename T2>
-ostream &operator<<(ostream &stream, const pair<T1, T2> &_p)
-{
-    stream << "<" << _p.first << ", " << _p.second << ">";
-    return stream;
-}
-
 #endif
 
-template <typename T>
-const T &min(const T &_x, const T &_y, const T &_z)
+namespace std
 {
-    return min(_x, min(_y, _z));
+    /**
+     * @brief This does exactly what you think it does
+     *
+     * @param _x The first value
+     * @param _y The second value
+     * @param _z The third value
+     * @return The minimum of the 3 values
+     */
+    template <typename T>
+    const T &min(const T &_x, const T &_y, const T &_z)
+    {
+        return min(_x, min(_y, _z));
+    }
+
+    /**
+     * @brief This does exactly what you think it does
+     *
+     * @param _x The first value
+     * @param _y The second value
+     * @param _z The third value
+     * @param _a The fourth value
+     * @return The minimum of the 4 values
+     */
+    template <typename T>
+    const T &min(const T &_x, const T &_y, const T &_z, const T &_a)
+    {
+        return min(_x, min(_y, _z, _a));
+    }
+
+    template <typename CharT, typename _ForwardIterator>
+    void __list_elements(basic_ostream<CharT> &stream, const _ForwardIterator &_begin, const _ForwardIterator &_end)
+    {
+        for (auto iter = _begin; iter != _end; iter++)
+        {
+            stream << *iter;
+            if (next(iter) != _end)
+            {
+                stream << ", ";
+            }
+        }
+    }
+
+    template <typename CharT, typename T, size_t N>
+    basic_ostream<CharT> &operator<<(basic_ostream<CharT> &stream, const array<T, N> &_v)
+    {
+        stream << "[";
+        __list_elements(stream, _v.begin(), _v.end());
+        stream << "]";
+
+        return stream;
+    }
+
+    template <typename CharT, typename T>
+    basic_ostream<CharT> &operator<<(basic_ostream<CharT> &stream, const vector<T> &_v)
+    {
+        stream << "[";
+        __list_elements(stream, _v.begin(), _v.end());
+        stream << "]";
+
+        return stream;
+    }
+
+    template <typename CharT, typename T>
+    basic_ostream<CharT> &operator<<(basic_ostream<CharT> &stream, const deque<T> &_v)
+    {
+        stream << "[";
+        __list_elements(stream, _v.begin(), _v.end());
+        stream << "]";
+
+        return stream;
+    }
+
+    template <typename CharT, typename T>
+    basic_ostream<CharT> &operator<<(basic_ostream<CharT> &stream, const list<T> &_l)
+    {
+        stream << "[";
+        __list_elements(stream, _l.begin(), _l.end());
+        stream << "]";
+
+        return stream;
+    }
+
+    template <typename CharT, typename T, typename Compare, typename Alloc>
+    basic_ostream<CharT> &operator<<(basic_ostream<CharT> &stream, const set<T, Compare, Alloc> &_s)
+    {
+        stream << "{";
+        __list_elements(stream, _s.begin(), _s.end());
+        stream << "}";
+
+        return stream;
+    }
+
+    template <typename CharT, typename T, typename Compare, typename Alloc>
+    basic_ostream<CharT> &operator<<(basic_ostream<CharT> &stream, const multiset<T, Compare, Alloc> &_s)
+    {
+        stream << "{";
+        __list_elements(stream, _s.begin(), _s.end());
+        stream << "}";
+
+        return stream;
+    }
+
+    template <typename CharT, typename T1, typename T2, typename Compare, typename Alloc>
+    basic_ostream<CharT> &operator<<(basic_ostream<CharT> &stream, const map<T1, T2, Compare, Alloc> &_m)
+    {
+        vector<pair<T1, T2>> _v(_m.begin(), _m.end());
+
+        stream << "{";
+        for (auto iter = _m.begin(); iter != _m.end(); iter++)
+        {
+            stream << iter->first << ": " << iter->second;
+            if (next(iter) != _m.end())
+            {
+                stream << ", ";
+            }
+        }
+        stream << "}";
+
+        return stream;
+    }
+
+    template <typename CharT, typename T1, typename T2, typename Compare, typename Alloc>
+    basic_ostream<CharT> &operator<<(basic_ostream<CharT> &stream, const unordered_map<T1, T2, Compare, Alloc> &_m)
+    {
+        vector<pair<T1, T2>> _v(_m.begin(), _m.end());
+
+        stream << "{";
+        for (auto iter = _m.begin(); iter != _m.end(); iter++)
+        {
+            stream << iter->first << ": " << iter->second;
+            if (next(iter) != _m.end())
+            {
+                stream << ", ";
+            }
+        }
+        stream << "}";
+
+        return stream;
+    }
+
+    template <typename CharT, typename T1, typename T2>
+    basic_ostream<CharT> &operator<<(basic_ostream<CharT> &stream, const pair<T1, T2> &_p)
+    {
+        return stream << "(" << _p.first << ", " << _p.second << ")";
+    }
 }
 
 signed main()
